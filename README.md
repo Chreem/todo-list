4b2ff0d3239e1f0722cb02b81c31b5ce0624cb0f
Chreem <549078191@qq.com>
2017-09-30 17:40:09 +0800

    feat: webComponent

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Components (storybook)
 
 ## vue
 
+storybook + 部分组件
+
 ## react
+
+甚至还有bug的todo-list
+
+## web-component
+
+原生组件
 
 ## design-patterns
---
+++ web-component/x-product.js
@@ -0,0 +1,36 @@
+class XProductProto extends HTMLElement {
+    createdCallback() {
+        // Create a Shadow Root
+        // var shadow = this.createShadowRoot();
+
+        // Create a standard img element and set it's attributes.
+        var img = document.createElement('img');
+        img.alt = this.getAttribute('data-name');
+        img.src = this.getAttribute('data-img');
+        img.width = '150';
+        img.height = '150';
+        img.className = 'product-img';
+
+        // Add the image to the Shadow Root.
+        this.appendChild(img);
+
+        // Add an event listener to the image.
+        img.addEventListener('click', function (e) {
+            window.location = this.getAttribute('data-url');
+        });
+
+        // Create a link to the product.
+        var link = document.createElement('a');
+        link.innerText = this.getAttribute('data-name');
+        link.href = this.getAttribute('data-url');
+        link.className = 'product-name';
+
+        // Add the link to the Shadow Root.
+        this.appendChild(link);
+    };
+}
+
+// Register the new element.
+var XProduct = document.registerElement('x-product', {
+    prototype: XProductProto.prototype
+});
